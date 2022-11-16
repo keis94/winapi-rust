8b6908d3d9c40cb36b61c2476dee00b701661fe2
keis94 <keis.vivi@gmail.com>
2022-11-16 17:55:25 +0900

    Split into function

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
+use std::ffi;
 use std::ptr;
+use windows::core;
+use windows::Win32;
 use windows::Win32::System::Registry::{self, RegOpenKeyExA, RegQueryValueExA};
-use windows::{s, Win32};
 
-fn main() {
+#[no_mangle]
+fn string_to_pcstr(s: &str) -> core::PCSTR {
+    // windows::s! only accepts literal (like "foo", 1), not &'static str.
+    // So manually converting from &str to PCSTR.
+    let s = ffi::CString::new(s).expect("CString::new() failes for &str with null byte.");
+    // pointer to str is no longer managed, so leak here.
+    // It should be freed with CString::from_raw()
+    core::PCSTR::from_raw(s.into_raw() as *const u8)
+}
+
+fn query_registry_value(
+    subkey: &str,
+    value: &str,
+    mut value_type: Registry::REG_VALUE_TYPE,
+) -> Result<String, Win32::Foundation::WIN32_ERROR> {
+    let mut hkey = Registry::HKEY(0);
+
     unsafe {
-        let mut hkey = Registry::HKEY(0);
-        let lpreserved: *mut u32 = ptr::null_mut();
-        let mut reg_value_type = Registry::REG_DWORD;
-        let mut lpcbdata: u32 = 0;
-
         let result = RegOpenKeyExA(
             Registry::HKEY_CURRENT_USER,
-            s!("Software\\Microsoft\\Windows\\CurrentVersion\\Run"),
+            string_to_pcstr(subkey),
             0,
             Registry::KEY_QUERY_VALUE,
             &mut hkey,
@@ -24,11 +37,13 @@ fn main() {
             )
         }
 
+        // Examine data size
+        let mut lpcbdata: u32 = 0;
         let result = RegQueryValueExA(
             hkey,
-            s!(""),
-            Some(lpreserved),
-            Some(&mut reg_value_type),
+            string_to_pcstr(value),
+            Some(ptr::null_mut()),
+            Some(&mut value_type),
             None,
             Some(&mut lpcbdata),
         );
@@ -39,24 +54,42 @@ fn main() {
                 error.message()
             )
         }
-        println!("success lpcbdata = {:?}", lpcbdata);
 
+        // Query value
         let mut lpdata = vec![0; lpcbdata as usize];
         let result = RegQueryValueExA(
             hkey,
-            s!(""),
-            Some(lpreserved),
-            Some(&mut reg_value_type),
+            string_to_pcstr(value),
+            Some(ptr::null_mut()),
+            Some(&mut value_type),
             Some(lpdata.as_mut_ptr()),
             Some(&mut lpcbdata),
         );
         match result {
             Win32::Foundation::NO_ERROR => {
-                println!("lpdata: {:?}", std::str::from_utf8(&lpdata).unwrap());
+                return Ok(String::from_utf8(lpdata).unwrap());
             }
             error => {
-                println!("error on RegQueryValueExA: {:?}", error);
+                return Err(error);
             }
+        }
+    }
+}
+
+fn main() {
+    let subkey = "Software\\Microsoft\\Windows\\CurrentVersion\\Run";
+    let value = "";
+    let value_type = Registry::REG_SZ;
+
+    match query_registry_value(subkey, value, value_type) {
+        Ok(d) => {
+            println!("subkey: {}", subkey);
+            println!("value: {}", value);
+            println!("data: {}", d);
+        }
+        Err(e) => {
+            let e = e.ok().unwrap_err();
+            println!("{} {}", e.code(), e.message())
         }
     }
 }
